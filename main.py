26e7d06b406ed1bd8d78619ae98b11436a37bf19
djohoe28 <djohoe28@gmail.com>
2023-01-06 23:40:02 +0200

    Added: * Chirp Generation
    Completed:
    * Change Sample Rate
    * Experiment with cross-referencing libraries
    * Basic Menu
    * Playback
    * Recording
    
    In Progress:
    * Change STFT window size
    
    Blocked:
    * Initial Database

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
 #  Copyright (c) 2022. Code by DJohoe28.
-
 import os
+from datetime import datetime
+from enum import Enum
 
 import librosa
 import librosa.display
 import matplotlib.pyplot as plt
 import numpy as np
+import sounddevice
+import sounddevice as sd
+import soundfile as sf
+
+# Initializations
+DEF_REC_PATH = "Recordings"  # Default Recordings Path
+DEF_DURATION = 1  # Default Duration (Chirps, Recordings, etc)
+DEF_SR = 44100  # Default Sample Rate (see above)
+sd.default.samplerate = DEF_SR  # Currently unused, as we know each file's sample rate
+sd.default.channels = 2  # Set sound-device's default to Stereo
 
 
-class Track (object):
+class Return(Enum):
+    SUCCESS = 1  # The function performed successfully.
+    CANCEL = 0  # The function was cancelled.
+    UNDEF_ERROR = -1  # All errors not mentioned below
+    INDEX_ERROR = -2  # Index (out of range) Error
+    VALUE_ERROR = -3  # Value (conversion) Error
+    AUDIO_ERROR = -4  # Audio (playback) Error
+
+
+class Track(object):
     """
     A Track loaded into the program.
 
@@ -50,7 +70,7 @@ class Track (object):
         """Sample rate of Track."""
 
         # Determine title.
-        if "/" not in self.path[0] and "\\" not in self.path[0]:
+        if path == "" or ("/" not in self.path[0] and "\\" not in self.path[0]):
             self.title = path  # File is saved locally.
         else:
             self.title = os.path.basename(self.path)
@@ -66,6 +86,19 @@ class Track (object):
     def __repr__(self: class_name):
         """A string representation of the Track."""
         return f"Track: {self.path}"
+
+    def play(self: class_name, wait: bool = True) -> None:
+        """
+        Play the Track.
+
+        :param wait: If true, the program halts until the Track stops playing once
+        :type wait: bool
+        :return: None
+        :rtype: NoneType
+        """
+        sd.play(self.y, self.sr)
+        if wait:
+            sd.wait()
 
     def plot(self: class_name, ax: plt.Axes = None) -> plt.Axes:
         """
@@ -95,12 +128,7 @@ class Track (object):
         return librosa.stft(self.y)
 
     def plot_stft(self: class_name):
-        """
-        Plot the STFT of the track in a new window.
-
-        :return: None
-        :rtype: NoneType
-        """
+        """Plot the STFT of the track in a new window."""
         db = librosa.amplitude_to_db(np.abs(self.get_stft()), ref=np.max)
         librosa.display.specshow(db, sr=self.sr, y_axis='log', x_axis='time')
 
@@ -131,9 +159,158 @@ class Track (object):
         return ax
 
 
-track1 = Track(librosa.ex('choice', hq=True))
-track2 = Track(librosa.example('nutcracker'))
-track1.plot_beat_times()  # TODO: Seems to miss beats near the end of the file. Intentional?
-plt.show()
-track2.plot_stft()
-plt.show()
+# Utilities
+def chirp_track(sr: int = DEF_SR, duration: float = DEF_DURATION) -> Track:
+    """
+    Creates a new Track instance with a synthetic chirp, going from C3 to C5.
+
+    :return: Track
+    :rtype: Track
+    """
+    track = Track(f"Chirp(sr={sr}, duration={duration})", load=False)  # Create an empty Track to record on.
+    track.sr = sr
+    track.y = librosa.chirp(fmin=librosa.note_to_hz('C3'),
+                            fmax=librosa.note_to_hz('C5'),
+                            sr=track.sr,
+                            duration=duration)
+    return track
+
+
+def example_load() -> None:
+    track1 = Track(librosa.ex('choice', hq=True))
+    track2 = Track(librosa.example('nutcracker'))
+    track1.plot_beat_times()  # TODO: Seems to miss beats near the end of the file. Intentional?
+    plt.show()
+    track2.plot_stft()
+    plt.show()
+
+
+# Main Program
+def main() -> Return:
+    """
+    The Main() function. Provides a user interface for the program.
+
+    :return: Did the function complete successfully?
+    :rtype: bool
+    """
+    # Variables
+    print("Hello! Welcome to the ACR Project!")
+    running = True
+    tracks = []
+
+    # Commands
+    def init() -> Return:
+        """Initialize a demo Track list."""
+        nonlocal tracks
+        tracks = [chirp_track(sr=sr) for sr in [44100, 48000]]  # Initialize Track list to Chirps of each SR in list.
+        return Return.SUCCESS
+
+    def terminate() -> Return:
+        """Terminate the program."""
+        nonlocal running
+        stop()
+        running = False
+        return Return.SUCCESS
+
+    def play() -> Return:
+        """Play a Track by ID from user."""
+        print("Available Tracks:")
+        for i in range(len(tracks)):
+            print(f"{i}: {tracks[i].title}")
+        track_id = input("Please enter Track ID:")
+        try:
+            track_id = int(track_id)
+            if track_id == "":
+                print("Cancelling...")
+                return Return.CANCEL
+            track: Track = tracks[track_id]
+            track.play(wait=True)
+            return Return.SUCCESS
+        except ValueError:
+            print("Not a valid Track ID number. Please try again.")
+            return Return.VALUE_ERROR
+        except IndexError:
+            print("Track ID out of range. Please try again.")
+            return Return.INDEX_ERROR
+        except sounddevice.PortAudioError:
+            print("Error playing Track. Please try again.")
+            return Return.AUDIO_ERROR
+        pass
+
+    def stop() -> Return:
+        """Stop playing all currently playing audios on sound-device."""
+        sd.stop()
+        return Return.SUCCESS
+
+    def record(wait: bool = True, save: bool = True, ext: str = "wav") -> Return:
+        """Record from sound-device."""
+        try:
+            duration = float(
+                input(f"Please enter recording Duration (seconds, Default={DEF_DURATION}):") or DEF_DURATION)
+        except ValueError:
+            print("Duration invalid. Please try again.")
+            return Return.VALUE_ERROR
+        try:
+            sr = int(input(f"Please enter Sample Rate (Hz, Default={DEF_SR}):") or DEF_SR)
+        except ValueError:
+            print("Sample Rate invalid. Please try again.")
+            return Return.VALUE_ERROR
+        recording = sd.rec(int(duration * sr))
+        if wait:
+            sd.wait()  # Wait for recording to finish.
+        track = Track(f'{DEF_REC_PATH}\\Recording - {datetime.now().strftime("%Y-%m-%d  %H-%M-%S")}', load=False)
+        track.sr = sr
+        track.y = recording
+        if save:
+            if not os.path.exists(DEF_REC_PATH):
+                os.mkdir(DEF_REC_PATH)  # Create "Recordings" folder if one does not exist. (SoundFile can't mkdir)
+            sf.write(f"{track.title}.{ext}", track.y, track.sr)  # Save recording to file.
+        # TODO: Soundfile takes a while to save the recording - is it possible to block Python in the meantime?
+        tracks.append(track)
+        return Return.SUCCESS
+
+    def generate() -> Return:
+        """Generate a new Chirp with input from user."""
+        nonlocal tracks
+        try:
+            sr = int(input("Please enter Chirp Sample Rate:"))
+        except ValueError:
+            print("Invalid Sample Rate. Please try again.")
+            return Return.VALUE_ERROR
+        try:
+            duration = float(input("Please enter Chirp Duration:"))
+        except ValueError:
+            print("Invalid Duration. Please try again.")
+            return Return.VALUE_ERROR
+        tracks.append(chirp_track(sr=sr, duration=duration))
+        return Return.SUCCESS
+
+    # Command List
+    cmds = dict()
+    for cmd in [init, terminate, play, stop, record, generate]:  # TODO: Add all commands here!
+        cmds[cmd.__name__] = cmd
+
+    # Main Loop
+    while running:
+        print("\nAvailable Commands:")
+        print(list(cmds.keys()))
+        inp = input("Please enter a command:").lower()  # Case-insensitive.
+        if inp not in cmds.keys():
+            print("Command not found. Please try again.")
+            continue
+        cmds[inp]()
+    print("Goodbye!")
+    return Return.SUCCESS
+
+
+if __name__ == "__main__":
+    main()
+
+# TODO:
+#  COMPLETE: Change Sample Rate -> Higher SR = shorter higher pitch
+#  COMPLETE: Experiment with cross-referencing librosa, soundfile, python-sounddevice -> All are based on NumPy.
+#  COMPLETE: Basic Menu -> Currently good-faith programmer-only.
+#  COMPLETE: Playback -> Not tested with relative/absolute path.
+#  COMPLETE: Recording (python-sounddevice = NumPy / pyaudio = byte) -> Used python-sounddevice, as it's based on NumPy.
+#  IN PROGRESS: Change STFT window size
+#  BLOCKED: Initial database -> Waiting to get home to rip CDs.
